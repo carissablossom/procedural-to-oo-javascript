b92beeabee8545719e7cadbbbbb8bb2779b645b6
Nicholas Toulouse <faintsoundofwater@gmail.com>
2015-06-21 11:35:20 -0700

    refactor code included

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,3 +1,19 @@
+// REFACTORING CODE
+
+
+// $(document).ready(function() {
+//   $('#roller button.add').on('click', function() {
+//     console.log("WAT")
+//     $('.dice').append('<div class="die">0</div>');
+//   });
+
+//   $('#roller button.roll').on('click', function() {
+//     $('.die').each(function(k, die) {
+//       var value = Math.floor((Math.random()*6)+1);
+//       $(die).text(value);
+//     });
+//   });
+// });
 
 // RELEASE 0
 // Provide good names for all anonymous functions.
@@ -142,5 +158,6 @@ var view = {
 }
 
 // MODEL
+
 
 
