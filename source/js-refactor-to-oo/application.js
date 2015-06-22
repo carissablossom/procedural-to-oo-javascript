eba8a9cfd9b03ab8f5d6e176bf7b0c149e7548c3
achen116 <chen.arielle@gmail.com>
2015-06-21 17:28:26 -0700

    initial refactor, make a dice js object

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,13 +1,31 @@
 $(document).ready(function() {
+
+  var dice = new Dice();
+  dice.add();
+  dice.roll();
+  
+});
+
+var Dice = function() {
+
+}
+
+Dice.prototype.add = function() {
+  
   $('#roller button.add').on('click', function() {
     console.log("WAT")
     $('.dice').append('<div class="die">0</div>');
   });
 
+}
+
+Dice.prototype.roll = function() {
+  
   $('#roller button.roll').on('click', function() {
     $('.die').each(function(k, die) {
       var value = Math.floor((Math.random()*6)+1);
       $(die).text(value);
     });
   });
-});
+
+}
