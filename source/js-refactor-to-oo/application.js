d7f14d099e1ef7ea5040b8d5b44ed1f38d13f1a1
Andrew Donato <andrew.donato91@gmail.com>
2015-06-21 23:42:27 -0700

    completed refactoring of dice challenge

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,13 +1,38 @@
-$(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
-  });
+// $(document).ready(function() {
+//   $('#roller button.add').on('click', function() {
+//     console.log("WAT")
+//     $('.dice').append('<div class="die">0</div>');
+//   });
 
-  $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
+//   $('#roller button.roll').on('click', function() {
+//     $('.die').each(function(k, die) {
+//       var value = Math.floor((Math.random()*6)+1);
+//       $(die).text(value);
+//     });
+//   });
+// });
+
+
+$(document).ready(function(){
+
+  var dice = new Dice();
+
+  $('#roller button.add').on('click', dice.add);
+  $('#roller button.roll').on('click', dice.roll);
+})
+
+var Dice = function() {
+
+}
+
+Dice.prototype.add = function(){
+  console.log("WAT")
+  $('.dice').append ('<div class="die">0</div>');
+}
+
+Dice.prototype.roll = function(){
+  $('.die').each(function(k, die){
+    var value = Math.floor((Math.random()*6) + 1);
+    $(die).text(value);
   });
-});
+}
