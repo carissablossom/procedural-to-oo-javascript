9ba9bce490ef97b62d5cf161525561a91033e4bd
cusackalex <cusackalex@gmail.com>
2015-06-22 22:24:37 -0700

    mvc refactor, having trouble rendering the new value though

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,32 +1,77 @@
 $(document).ready(function() {
 
-  var bindRoller = function(){
-    $('#roller button.add').on('click', createDie)
+  // var bindRoller = function(){
+  //   $('#roller button.add').on('click', createDie)
+  // };
+
+  // var createDie = function(){
+  //   console.log("WAT -- ROOLLING DICE")
+  //   $('.dice').append('<div class="die">0</div>');
+  // };
+
+
+  // var rollDice = function(){
+  //   $('.die').each(randomNumber)
+  // };
+
+  // var randomNumber = function (k, die){
+  //   var value = Math.floor((Math.random()*6)+1);
+  //    $(die).text(value);
+  // }
+
+  // var bindNewDice = function(){
+  //   $('#roller button.roll').on('click', rollDice)
+  // }
+
+
+
+//view
+
+  var Die = function(){
+    this.number = 8;
   };
 
-  var createDie = function(){
-    console.log("WAT -- ROOLLING DICE")
-    $('.dice').append('<div class="die">0</div>');
-  };
-
-
-  var rollDice = function(){
-    $('.die').each(randomNumber)
-  };
-
-  var randomNumber = function (k, die){
-    var value = Math.floor((Math.random()*6)+1);
-     $(die).text(value);
+  var Game = function(){
+    this.array = [];
   }
 
-  var bindNewDice = function(){
-    $('#roller button.roll').on('click', rollDice)
-  }
+  Game.prototype.newDie = function (){
+    die = new Die;
+    game.array.push(die);
+    appendDie(die);
+  };
 
+  Game.prototype.rollDice = function(){
+    for(i=0; i < game.array.length; i++){
+      console.log("rolling dice")
+      die = game.array[i]
+      die.renderRoll(die, Math.floor((Math.random()*6)+1));
+    };
+  };
 
-  bindNewDice();
+  var appendDie = function(die){
+    $('.dice').append('<div class="die">'+die.number+'</div>');
+  };
 
-  bindRoller();
+  Die.prototype.renderRoll = function (die, value) {
+     die.number = 1;
+  };
 
+  var bindRoller = function(game){
+    $('#roller button.roll').on('click', game.rollDice)
+  };
+
+  var bindNewDice = function(game){
+    $('#roller button.add').on('click', game.newDie)
+  };
+
+  Game.prototype.play = function(){
+    game: new Game;
+    bindNewDice(this);
+    bindRoller(this);
+  };
+//controller
+  game = new Game;
+  game.play()
 
 });
