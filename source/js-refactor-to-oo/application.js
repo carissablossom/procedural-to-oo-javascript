6781aa96750e5f9a40446e89b776aea92fe02c4f
Jeff Chang <jeffrey.chang2014@gmail.com>
2015-06-23 12:05:25 -0700

    Primary functionality works. Working on MVC separation

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,13 +1,70 @@
 $(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
+  dice = new Dice;
+  view = new View;
+  $('#roller button.add').on('click',function() {
+    dice.addDie();
+    view.drawNewDie(dice);
   });
-
-  $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
-  });
+  $('#roller button.roll').on('click',function() {
+    dice.rollAllDie();
+    view.updateDie(dice);
+  })
 });
+
+//Model
+var Dice = function() {
+  this.collection = [];
+};
+
+Dice.prototype.addDie = function() {
+  this.collection.push(new Die);
+};
+
+Dice.prototype.rollAllDie = function() {
+  var a = this.collection
+  for (var i= 0; i<a.length;i++){
+    a[i].roll();
+  };
+};
+
+var Die = function() {
+  this.value = 0;
+};
+
+Die.prototype.roll = function() {
+  this.value = Math.floor((Math.random()*6)+1);
+  return this.value;
+};
+
+//View
+var View = function(){};
+
+View.prototype.drawNewDie = function(dice){
+  var newDie = '<div class="die" id="'+(dice.collection.length-1)+'">0</div>'
+  $('.dice').append(newDie);
+};
+
+View.prototype.updateDie = function(dice){
+  $('.die').each(function(k,die) {
+    var object = dice.collection[die["id"]]
+    $(die).text(object.value);
+  });
+};
+
+
+//Controller
+var Controller = function(){
+  this.dice = new Dice;
+};
+
+ // $('#roller button.add').on('click', function() {
+ //    console.log("WAT")
+ //    $('.dice').append('<div class="die">0</div>');
+ //  });
+
+ //  $('#roller button.roll').on('click', function() {
+ //    $('.die').each(function(k, die) {
+ //      var value = Math.floor((Math.random()*6)+1);
+ //      $(die).text(value);
+ //    });
+ //  });
