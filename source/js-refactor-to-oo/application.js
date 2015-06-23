34ba53171d27e0341c932782ad914771a82d5e9a
Jared Rader <raderj89@gmail.com>
2015-06-22 19:56:09 -0700

    oo'd die

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,13 +1,50 @@
-$(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
-  });
+var diceApp = {};
 
-  $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
+diceApp.View = function(selector) {
+  this.element = selector || $('#roller');
+  this.addButton = this.element.find('button.add');
+  this.rollButton = this.element.find('button.roll');
+};
+
+diceApp.Die = function(sides) {
+  this.sides = sides || 6;
+};
+
+diceApp.Die.prototype.roll = function() {
+  return Math.floor((Math.random()*this.sides)+1);
+};
+
+diceApp.Controller = function(options) {
+  this.view = options.view;
+  this.die = options.model;
+};
+
+diceApp.Controller.prototype.addDice = function(selector) {
+  var controller = this;
+  var diceContainer = selector;
+
+  controller.view.addButton.on('click', function() {
+    diceContainer.append('<div class="die">0</div>');
+  });
+};
+
+diceApp.Controller.prototype.rollDice = function() {
+  var controller = this;
+
+  controller.view.rollButton.on('click', function() {
+    controller.view.element.find('.die').each(function(k, die) {
+      $(die).text(controller.die.roll());
     });
   });
+};
+
+
+$(document).ready(function() {
+  app = new diceApp.Controller({
+    view: new diceApp.View(),
+    model: new diceApp.Die(6)
+  });
+
+  app.addDice($('.dice'));
+  app.rollDice();
 });
