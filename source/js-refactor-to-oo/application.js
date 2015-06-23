006fe31de51238a57ff0c6d1441ca48a65541b85
cusackalex <cusackalex@gmail.com>
2015-06-22 21:27:53 -0700

    refactor functions into 5 seperate functions

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,13 +1,32 @@
 $(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
-  });
 
-  $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
-  });
+  var bindRoller = function(){
+    $('#roller button.add').on('click', createDie)
+  };
+
+  var createDie = function(){
+    console.log("WAT -- ROOLLING DICE")
+    $('.dice').append('<div class="die">0</div>');
+  };
+
+
+  var rollDice = function(){
+    $('.die').each(randomNumber)
+  };
+
+  var randomNumber = function (k, die){
+    var value = Math.floor((Math.random()*6)+1);
+     $(die).text(value);
+  }
+
+  var bindNewDice = function(){
+    $('#roller button.roll').on('click', rollDice)
+  }
+
+
+  bindNewDice();
+
+  bindRoller();
+
+
 });
